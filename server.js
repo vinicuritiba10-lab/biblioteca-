434889f36ff85782ae1cd9a112077438414b7461
stravoia <vinicuritiba10@gmail.com>
2026-03-23 01:59:03 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,36 +2,67 @@ const express = require("express");
 
 const server = express();
 const port = 3000;
+const usuarios = require("./models/usuarios");
+const bodyParser = require("body-parser");
 
-server.get("/", (req, res) =>{
-	res.send("tudo funcionando");
+//config bodyparser
+server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.json());
+
+server.post("/usuarios", function(req, res){
+	usuarios.create({
+		nome: req.body.nome,
+		email: req.body.email,
+		senha: req.body.senha,
+		tipo: req.body.tipo,
+		matricula: req.body.matricula
+
+
+	}).then(function(){
+		res.send("usuario cadastrado com sucesso");
+	}).catch(function(erro){
+		res.send("erro ao cadastrar usuario" + erro);
+	});
 });
 
-server.get("/login", (req, res) =>{
-	res.send("");
+server.get("/", function(req, res){
+	usuarios.findAll().then(function(usuarios){
+		res.send({usuarios: usuarios})
+	}).catch(function(erro){
+		res.send("erro ao buscar dados" + erro)
+	})
 });
 
-server.get("/emprestimos", (req, res) =>{
-	res.send("");
+server.get("/:nome", function(req,res){
+	usuarios.findAll({where: {"nome": req.params.nome}}).then(function(usuarios){
+		res.send(usuarios);
+	}).catch(function(erro){
+		res.send("usuario nao existe na base de dados" + erro);
+	})
+})
+
+server.patch("/atualizar/:id", function(req,res){
+	usuarios.update({
+		nome: req.body.nome,
+		email: req.body.email,
+		senha: req.body.senha,
+		tipo: req.body.tipo,
+		matricula: req.body.matricula},
+		{where: {"id": req.params.id}}
+	).then(function(){
+		res.send("sucesso ao atualizar os dados do produto ");
+	}).catch(function(erro){
+		res.send("erro ao atualizarr os dados do produto" + erro);
+	});
 });
 
-server.get("/livros", (req, res) =>{
-	res.send("");
+server.delete("/deletar/:id",function(req,res){
+	usuarios.destroy({where:{id: req.params.id}}).then(function(){
+		res.send("usuario deleatado com sucesso")
+	}).catch(function(erro){
+		res.send("erroa ao deletar usuario" + erro);
+	});
 });
-
-server.get("/devolutivas", (req, res) =>{
-	res.send("");
-});
-
-server.get("/aluno", (req, res) =>{
-	res.send("");
-});
-
-server.get("/bibliotecario", (req, res) =>{
-	res.send("");
-});
-
-
 
 server.listen(port, () =>{
 	console.log(`example app listening on port ${port}`);
