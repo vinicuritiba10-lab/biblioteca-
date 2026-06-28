da00a6c2240efc0b37009fcde65272d20a0175e8
vinicuritiba10-lab <vinicuritiba10@gmail.com>
2026-06-28 00:00:06 -0300

    teste

--- TR/home/home.js
+++ TR/home/home.js
@@ -356,12 +356,12 @@ async function mostrarMeusEmprestimos() {
         
         return `
             <div class="book-card">
-            <!-- <div class="book-cover ${cores[index % cores.length]}">${icones[index % icones.length]}</div> -->
-            <div class="book-cover ${cores[index % cores.length]}" ${livro.capa_url ? 'style="padding:0;"' : ''}>
-                ${livro.capa_url
-                    ? `<img src="${livro.capa_url}" alt="Capa de ${livro.titulo}" class="book-cover-img" onerror="this.parentElement.innerHTML='${icones[index % icones.length]}'">`
-                    : icones[index % icones.length]}
-            </div>
+                <!-- <div class="book-cover ${cores[index % cores.length]}">${icones[index % icones.length]}</div> -->
+                <div class="book-cover ${cores[index % cores.length]}" ${livro.capa_url ? 'style="padding:0;"' : ''}>
+                    ${livro.capa_url
+                        ? `<img src="${livro.capa_url}" alt="Capa de ${livro.titulo}" class="book-cover-img" onerror="this.parentElement.innerHTML='${icones[index % icones.length]}'">`
+                        : icones[index % icones.length]}
+                </div>
                 <div class="book-info">
                     <h3>${emp.livro.titulo}</h3>
                     <p>Autor: ${emp.livro.autor}</p>
