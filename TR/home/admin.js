17ba61fdc3bd506e643bfbdaf9dffde3103d2caf
vinicuritiba10-lab <vinicuritiba10@gmail.com>
2026-06-29 13:12:48 -0300

    teste

--- TR/home/admin.js
+++ TR/home/admin.js
@@ -283,7 +283,7 @@ async function adicionarLivro(event) {
         categoria: document.getElementById('categoria').value,
         quantidade_total: parseInt(document.getElementById('quantidade_total').value) || 1,
         capa_url: document.getElementById('capa_url').value || null,
-        descricao: document.getElementById('texto-da-sinopse').value
+        descricao: document.getElementById('notificao-sinopse').value
     };
 
     const estaEditando = livroEmEdicaoId !== null;
