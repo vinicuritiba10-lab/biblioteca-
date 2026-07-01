0008b7cf4e25c9a6e458da8da3a03e810edba882
vinicuritiba10-lab <vinicuritiba10@gmail.com>
2026-07-01 17:09:09 -0300

    teste

--- TR/email/config/email.js
+++ TR/email/config/email.js
@@ -5,8 +5,6 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
