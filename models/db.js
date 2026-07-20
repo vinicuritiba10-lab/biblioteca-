658f3c2e027d31c930eb071a0b4c68efb7113e56
stravoia <vinicuritiba10@gmail.com>
2026-07-20 18:14:26 -0300

    Add files via upload

--- models/db.js
+++ models/db.js
@@ -1,21 +1,19 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.MYSQLDATABASE || "biblioteca",
-    process.env.MYSQLUSER || "root",
-    process.env.MYSQLPASSWORD || "123456",
-    {
-        host: process.env.MYSQLHOST || "localhost",
-        dialect: "mysql",
-        port: process.env.MYSQLPORT || 3306
-    }
-);
-
-sequelize.authenticate().then(function(){
-    console.log("banco de dados conectado com sucesso");
-}).catch(function(erro){
-    console.log("erro ao conectar banco de dados: " + erro);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    logging: false
 });
+
+sequelize.authenticate()
+    .then(() => console.log('banco de dados conectado com sucesso'))
+    .catch(erro => console.log('erro ao conectar banco de dados: ' + erro));
 
 module.exports = {
     Sequelize: Sequelize,
