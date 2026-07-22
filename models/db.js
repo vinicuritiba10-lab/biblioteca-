7f4620393bbb1541c663b5b0a45e46716477aee5
vinicuritiba10-lab <vinicuritiba10@gmail.com>
2026-07-21 22:27:00 -0300

    teste

--- models/db.js
+++ models/db.js
@@ -3,9 +3,9 @@ const dns = require('dns');
 dns.setDefaultResultOrder('ipv4first');
 
 const sequelize = new Sequelize(
-    'postgres',           // nome do banco
-    'postgres',           // usuário
-    process.env.DB_PASS,  // só a senha
+    'postgres',
+    'postgres',
+    process.env.DB_PASS,
     {
         host: 'db.flttbrxdpqmshzkukfnl.supabase.co',
         port: 5432,
@@ -19,3 +19,12 @@ const sequelize = new Sequelize(
         logging: false
     }
 );
+
+sequelize.authenticate()
+    .then(() => console.log('banco de dados conectado com sucesso'))
+    .catch(erro => console.log('erro ao conectar banco de dados: ' + erro));
+
+module.exports = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+};
