3fa13829e9a9a0e9ec2818c44df0c166c3c5d4b8
vinicuritiba10-lab <vinicuritiba10@gmail.com>
2026-07-21 22:50:53 -0300

    teste

--- models/db.js
+++ models/db.js
@@ -2,23 +2,15 @@ const Sequelize = require('sequelize');
 const dns = require('dns');
 dns.setDefaultResultOrder('ipv4first');
 
-const sequelize = new Sequelize(
-    'postgres',
-    'postgres',
-    process.env.DB_PASS,
-    {
-        host: 'db.flttbrxdpqmshzkukfnl.supabase.co',
-        port: 5432,
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        },
-        logging: false
-    }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: process.env.DATABASE_URL.includes('supabase')
+            ? { require: true, rejectUnauthorized: false }
+            : false
+    },
+    logging: false
+});
 
 sequelize.authenticate()
     .then(() => console.log('banco de dados conectado com sucesso'))
